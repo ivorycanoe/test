da5cbdc924f64dd8ca441462f2b1d03255d0ac2a
jingalaxy <jingalaxy@163.com>
2021-02-25 10:25:31 +0800

    commit

---
+++ test.R
@@ -0,0 +1,11 @@
+print("This file was created within RStudio")
+
+print("And now it lives on GitHub")
+print("And now it lives on GitHub")
+
+sayHello<-function(name){
+  print(paste("hello",name))
+}
+
+sayHello("Conan")
+sayHello("World")
